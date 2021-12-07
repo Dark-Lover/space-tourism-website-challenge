9bfe7218426d36f7c688196d63b3f7ebd7b8f281
“Houssine” <person.zen15@gmail.com>
2021-12-08 00:47:05 +0100

    Finish styling mobile Crew Comp

--- src/components/crew/Crew.js
+++ src/components/crew/Crew.js
@@ -13,20 +13,36 @@ const Crew = () => {
   const myImages = importAll(secRequire);
   const { crew } = data;
   const [member, setMember] = useState(0);
-
+  crew.map((el) => console.log(el.name));
   return (
     <header>
       <Navbar />
-      <div className="crew_hero">
+      <div className="crew_hero flex column">
         <div className="m_heading">
           <span>02</span> Meet your crew
         </div>
-        <div className="content">
+        <div className="content flex column">
           <div className="img_box">
             <img
               src={myImages[`${crew[member].images.png}.png`].default}
               alt=""
             />
+          </div>
+          <div className="crew_infos flex column">
+            <div className="slide_bullets flex row">
+              {crew.map((el, index) => (
+                <div
+                  className={`bullet ${index === member ? "active" : ""}`}
+                  key={el.name}
+                  onClick={() => setMember(index)}
+                ></div>
+              ))}
+            </div>
+            <div className="details flex column">
+              <div className="crew_job role">{crew[member].role}</div>
+              <div className="crew_name heading">{crew[member].name}</div>
+              <div className="crew_desc lead">{crew[member].bio}</div>
+            </div>
           </div>
         </div>
       </div>
